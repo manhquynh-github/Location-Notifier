a5425e5e6016b03fda74745e1fbc3fde9bc22d8c
Đức Tiến <ductien.uit@gmail.com>
2018-10-30 18:07:58 +0700

    merge initialize state to current state in local storage

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { createStore } from 'redux';
 import combinedReducer from '../reducers/combinedReducer';
 import {persistReducer,persistStore} from 'redux-persist'
 import {AsyncStorage} from 'react-native'
+import autoMergeLevel1  from 'redux-persist/lib/stateReconciler/hardSet'
 
 const persistConfig ={
     key:'root',
     storage:AsyncStorage,
+    stateReconciler:autoMergeLevel1, 
 };
 
 const persistedReducer = persistReducer(persistConfig,combinedReducer);
